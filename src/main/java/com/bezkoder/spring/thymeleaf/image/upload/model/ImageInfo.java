04862d48b91ffb3b446f6c1dcf00ce03b2a4e741
Amanbadshah <100015352+Amanbadshah@users.noreply.github.com>
2024-10-17 17:26:11 +0530

    Basic

--- src/main/java/com/bezkoder/spring/thymeleaf/image/upload/model/ImageInfo.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/image/upload/model/ImageInfo.java
@@ -4,6 +4,8 @@ public class ImageInfo {
   
   private String name;
   private String url;
+  private String username;
+  private String password = "HardcodedPassword123"; // Intentional leak
 
   public ImageInfo(String name, String url) {
     this.name = name;
@@ -24,5 +26,23 @@ public class ImageInfo {
 
   public void setUrl(String url) {
     this.url = url;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  // Optionally, you can include a setter for password, 
+  // but be careful with exposing it since it's a leak
+  public void setPassword(String password) {
+    this.password = password;
   }
 }
